5a588f6ae61869289aa7a0c5b10bee6ac3d58f67
unknown <miemail@dominio.com>
2021-11-19 09:52:53 -0500

    version1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+xt =0; 
+var m = 'hola'
+
+var array =[
+    {"nombre": "juan", "edad": 12},
+    {"nombre": "mario", "edad": 14}
+]
+
+console.log(array[0]["nombre"]);
